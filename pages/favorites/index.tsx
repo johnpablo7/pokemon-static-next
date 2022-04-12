f41a4c235f9e356e482cc4994552f793fec8e6a3
john <john.davis.2015.03@gmail.com>
2022-04-11 22:07:07 -0500

    68. Librerías externas - Canvas-Confetti

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+
 import { Layout } from "../../components/layouts";
+import { localFavorites } from "../../utils";
+import { NoFavorites } from "../../components/ui";
+import { FavoritePokemons } from "../../components/pokemon";
 
 const FavoritesPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
   return (
     <Layout title="Pokémon - Favoritos">
-      <h1>Favoritos</h1>
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <FavoritePokemons pokemons={favoritePokemons} />
+      )}
     </Layout>
   );
 };
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,19 +1,45 @@
-import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+import { useState } from "react";
+
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+
+import confetti from "canvas-confetti";
 
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
 import { Pokemon } from "../../interfaces";
+import { localFavorites } from "../../utils";
 
 interface Props {
   pokemon: Pokemon;
 }
 
 const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+  const [isInFavorites, setIsInFavorites] = useState(
+    localFavorites.existInFavorites(pokemon.id)
+  );
+
   // console.log(pokemon);
+  const onToggleFavorite = () => {
+    localFavorites.toggleFavorite(pokemon.id);
+    setIsInFavorites(!isInFavorites);
+
+    if (isInFavorites) return;
+
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0,
+      },
+    });
+  };
 
   return (
-    <Layout title="Algun pokemon">
+    <Layout title={pokemon.name}>
       <Grid.Container css={{ marginTop: "5px" }} gap={2}>
         <Grid xs={12} sm={4}>
           <Card hoverable css={{ padding: "30px" }}>
@@ -39,8 +65,12 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
               <Text h1 transform="capitalize">
                 {pokemon.name}
               </Text>
-              <Button color="gradient" ghost>
-                Guardar En Favoritos
+              <Button
+                color="gradient"
+                ghost={!isInFavorites}
+                onClick={onToggleFavorite}
+              >
+                {isInFavorites ? "Quitar de Favoritos" : "Guardar En Favoritos"}
               </Button>
             </Card.Header>
             <Card.Body>
@@ -81,7 +111,6 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 
 // Debe usar getStaticPaths si está pre-renderizando estáticamente páginas que usan rutas dinámicas
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  // su función de búsqueda aquí
   const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
   // console.log({ pokemons151 });
 

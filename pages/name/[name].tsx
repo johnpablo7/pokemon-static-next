64f78d3bb6620e9ab202e7a82cedf580710dba72
john <john.davis.2015.03@gmail.com>
2022-04-12 22:49:57 -0500

    Fin de la Sección 6 Incremental Static Generation (ISG)

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -120,16 +120,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemonNames.map((name) => ({
       params: { name },
     })),
-    fallback: false,
+    // paths: [],
+    // fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
 
+  const pokemon = await getPokemonInfo(name.toLocaleLowerCase());
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name),
+      pokemon,
     },
   };
 };
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -1,12 +1,21 @@
 import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
+import axios from "axios";
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    }
+
+    return null;
+  }
 };
